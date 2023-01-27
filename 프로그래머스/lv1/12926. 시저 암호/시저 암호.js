867cd6721f9cdefa1d37837852064542a573370f
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-01-27 19:14:38 +0900

    [level 1] Title: 시저 암호, Time: 2.12 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,74 +1,16 @@
-function solution(s = 'z', n = 1) {
-  const lower = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
-  const upper = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ];
-  const arr = [];
-  for (let i = 0; i < s.length; i++) {
-    if (lower.includes(s[i])) {
-      const lowIndex = lower.indexOf(s[i]) + n;
-      const lowCase = lowIndex > 25 ? lowIndex - 26 : lowIndex;
-      arr.push(lower[lowCase]);
-    } else if (upper.includes(s[i])) {
-      const upperIndex = upper.indexOf(s[i]) + n;
-      const lowCase = upperIndex > 25 ? upperIndex - 26 : upperIndex;
-      arr.push(upper[lowCase]);
-    } else {
-      arr.push(' ');
-    }
-  }
+function solution(s, n) {
+  const low = 'abcdefghijklmnopqrstuvwxyz';
+  const up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  return arr.join('');
+  const answer = [...s].map(v => {
+    const indexofLow = low.indexOf(v);
+    const indexofUp = up.indexOf(v);
+
+    const sendLow = indexofLow + n > 25 ? indexofLow + n - 26 : indexofLow + n;
+    const sendUp = indexofUp + n > 25 ? indexofUp + n - 26 : indexofUp + n;
+
+    return low.includes(v) ? low[sendLow] : up.includes(v) ? up[sendUp] : ' ';
+  });
+
+  return answer.join('');
 }
