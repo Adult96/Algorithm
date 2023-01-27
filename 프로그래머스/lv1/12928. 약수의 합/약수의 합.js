33358fa01e73391e96bd5f6159bb79098f2cb78d
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-01-27 21:15:34 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,10 @@
-const solution = (n = 12) => {
-  let num = 0;
+function solution(n = 12) {
+  let answer = 0;
   for (let i = 1; i <= n; i++) {
     if (n % i === 0) {
-      num += i;
+      answer += i;
     }
   }
-  return num;
-};
+
+  return answer;
+}
