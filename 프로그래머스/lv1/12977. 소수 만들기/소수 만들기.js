745818afad9458e709b7e258f7cf7a8c50354af6
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-01-27 18:08:06 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,20 +1,21 @@
-function solution(nums = [1, 2, 3, 4, 5, 6]) {
-  let answer = 0;
-  let once = 0;
+function solution(nums = [1, 2, 7, 6, 4]) {
+  let sum = 0;
+  let cnt = 0;
+
   for (let i = 0; i < nums.length - 2; i++) {
     for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
-        once = nums[i] + nums[j] + nums[k];
-        console.log(nums[i], nums[j], nums[k]);
-        answer++;
-        for (let n = 2; n <= Math.sqrt(once); n++) {
-          if (once % n === 0) {
-            answer--;
+        sum = nums[i] + nums[j] + nums[k];
+        cnt += 1;
+        for (let l = 2; l <= Math.sqrt(sum); l++) {
+          if (sum % l === 0) {
+            cnt -= 1;
             break;
           }
         }
       }
     }
   }
-  return answer;
+
+  return cnt;
 }
