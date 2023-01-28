e9f520a22556a901810788317c298f85ac1dd15b
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-01-28 10:27:16 +0900

    [level 1] Title: 소수 찾기, Time: 222.59 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n = 1000000) {
+  let cnt = 0;
+  for (let i = 2; i <= n; i++) {
+    checkIsPrimeNumber(i) && cnt++;
+  }
+  return cnt;
+}
+
+function checkIsPrimeNumber(num) {
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
