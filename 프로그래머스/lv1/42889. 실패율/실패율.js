af877f0c44eed03df2b5f295e1bbf01bbf3e7165
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-01-28 11:14:33 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,7 +1,6 @@
 function solution(N = 5, stages = [2, 1, 2, 6, 2, 4, 3, 3]) {
   let arr = [stages];
   let rank = [];
-  stages.sort();
 
   for (let i = 1; i <= N; i++) {
     const stage = arr[i - 1].filter(v => v > i);
@@ -10,7 +9,7 @@ function solution(N = 5, stages = [2, 1, 2, 6, 2, 4, 3, 3]) {
     const failStatus = (arr[i - 1].length - arr[i].length) / arr[i - 1].length;
     rank.push([failStatus, i]);
   }
-  
-  rank = rank.sort((a, b) => b[0] - a[0]).map(v => v[1]);
-  return rank;
+
+  rank.sort((a, b) => b[0] - a[0]);
+  return rank.map(v => v[1]);
 }
