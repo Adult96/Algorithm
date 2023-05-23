2c2510bda6751cacab2686893e194e3039ef9900
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-05-23 17:56:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.52 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,2 +1,3 @@
-const solution = (arr = [1, 1, 3, 3, 0, 1, 1]) =>
-  arr.filter((v, i) => v !== arr[i + 1]);
+function solution(arr = [1, 1, 3, 3, 0, 1, 1]) {
+  return arr.filter((v, i, arr) => v !== arr[i + 1]);
+}
