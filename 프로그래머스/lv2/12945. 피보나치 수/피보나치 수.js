659c6929c1532188fca03f31c867dfde2e6f02e9
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-05-31 15:57:05 +0900

    [level 2] Title: 피보나치 수, Time: 34.29 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n = 5) {
+  let answer = [1, 2];
+
+  for (let i = 2; i < n - 1; i++) {
+    answer.push((answer[i - 1] + answer[i - 2]) % 1234567);
+  }
+  return answer[n - 2];
+}
