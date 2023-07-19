bbfb4b8d448d26682b681b0ccfd2a91215ea293e
Adult96 <103473925+Adult96@users.noreply.github.com>
2023-07-19 16:20:54 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,11 +1,11 @@
-const solution = (a = 10, b = 2) => {
-  const max = a + b;
+function solution(brown, yellow) {
+  const max = brown + yellow;
 
-  for (let i = 3; i < a; i++) {
+  for (let i = 3; i < brown; i++) {
     if (max % i !== 0) continue;
 
     const x = max / i;
     const y = i;
-    if ((x - 2) * (y - 2) === b) return [x, y];
+    if ((x - 2) * (y - 2) === yellow) return [x, y];
   }
-};
+}
